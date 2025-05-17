2e2112285f66b2b44a2eb8b881fe64bf916879b0
shahidterra65 <shahidterra65@gmail.com>
2025-05-17 07:08:17 -0700

    added variable file

--- main.tf
+++ main.tf
@@ -10,19 +10,3 @@ resource "aws_instance" "web" {
     Name = var.Name
   }
 }
-variable "os" {
-  description = "this default ami is for us-east-1 region"
-  type        = string
-  default     = "ami-0e449927258d45bc4"
-}
-variable "Size" {
-  default = "t2.micro"
-
-}
-variable "Name" {
-default = "SonarQube"
-}
-variable "counts" {
- default = 1
-
-}
